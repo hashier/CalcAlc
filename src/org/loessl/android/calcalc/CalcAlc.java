b5f29f243c44f0440769297c7878244bc149fd9a
Christopher Loessl <cloessl@x-berg.de>
2010-02-16 18:10:31 +0100

    GUI done

---
+++ src/org/loessl/android/calcalc/CalcAlc.java
@@ -0,0 +1,13 @@
+package org.loessl.android.calcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcAlc extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
